44eddb11a892cadc3ccafa28c584105ce2af25b4
deankevorkian <deankevorkian@gmail.com>
2018-11-10 17:11:29 +0200

    Renamed init script. Added sections for initializing other tables'

---
+++ arsenal/event_to_user.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[event_to_user]
+(
+	[event_id] INT NOT NULL PRIMARY KEY, 
+    [user_name] NCHAR(20) NOT NULL, 
+    CONSTRAINT [FK_event_to_user_username] FOREIGN KEY ([user_name]) REFERENCES [dbo].[Users], 
+    CONSTRAINT [FK_event_to_user_eventid] FOREIGN KEY ([event_id]) REFERENCES [dbo].[Events]
+)
